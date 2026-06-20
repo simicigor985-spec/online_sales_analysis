a9367d50c21bff8ecfe4f869b6830a819efa9680
Igor <simicigor985@gmail.com>
2026-06-20 16:38:37 +0200

    Added remove product

--- product_menager.py
+++ product_menager.py
@@ -20,3 +20,11 @@ class ProductManager:
         total_value = sum(
             product.price * product.quantity for product in self.products)
         return print(f"Total Inventory Value: ${total_value:.2f}")
+
+    def remove_product(self, product_name):
+        for product in self.products:
+            if product.name == product_name:
+                self.products.remove(product)
+                print(f"Removed {product_name} from inventory.")
+                return
+        print(f"Product {product_name} not found in inventory.")
